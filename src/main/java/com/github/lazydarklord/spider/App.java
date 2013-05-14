5c92a2bc07dc11f757443c3815c2dae207f7ebd6
Sudheendra Kumar <sudhi@sudhi-VirtualBox.(none)>
2013-05-15 02:13:17 +1200

    Importing base maven project

---
+++ src/main/java/com/github/lazydarklord/spider/App.java
@@ -0,0 +1,13 @@
+package com.github.lazydarklord.spider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
