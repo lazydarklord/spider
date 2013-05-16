2c399b3541a8838d7e6c8bafd11c7f34a8324d65
sudhi <sudhi@sudhi-VirtualBox>
2013-05-17 07:17:33 +1200

    TripTest removed

--- src/test/java/com/github/lazydarklord/spider/test/AllTests.java
+++ src/test/java/com/github/lazydarklord/spider/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({AppTest.class, GraphTest.class, JourneyPlannerTest.class, TripTest.class})
+@SuiteClasses({AppTest.class, GraphTest.class, JourneyPlannerTest.class})
 public class AllTests
 {
 
